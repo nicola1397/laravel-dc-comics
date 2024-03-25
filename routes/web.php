2f6d600c0256ec5aa91fda28cabaa90257113e57
nicola1397 <nicola1397@outlook.it>
2024-03-25 19:56:32 +0100

    feat: added -msrc

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-  return view('home');
-})->name('home');
+Route::resource("comics", ComicController::class);
